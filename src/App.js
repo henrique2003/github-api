2a9f1ebb541d06bf3aabaabd2d0ef5969fd5140d
Henrique de melo <henrique.de.melo.cristioglu@gmail.com>
2020-05-28 18:54:20 -0300

    style: card

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
+import './App.css'
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper_app">
+      <div className="wrapper_title">
+        <h1>Github-Api</h1>
+      </div>
+      <div className="container">
+        <div className="card">
+          <div className="card_header">
+            <div className="wrapper_avatar">
+              <img src="https://avatars2.githubusercontent.com/u/56162187?s=460&u=d8ffcbc88a354b9d26754d7f2e871ebd8e6d60b0&v=4" alt="" />
+            </div>
+            <div className="wrapper_content">
+              <strong>Henrique de Melo</strong><br />
+              <span>henrique2003</span>
+            </div>
+          </div>
+          <div className="card_body">
+            <p>Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsumLorem ipsumLorem ipsum</p>
+          </div>
+          <div className="card_footer">
+            <button>Editar</button>
+            <button>Apagar</button>
+          </div>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
